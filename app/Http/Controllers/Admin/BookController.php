48bcb1f222db9fed0e11df787f48a7866e210207
sulkifli27 <sulgameranimasi1@gmail.com>
2023-01-18 19:14:20 +0800

    add crud laravel

---
+++ app/Http/Controllers/Admin/BookController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Models\Book;
+use Illuminate\Http\Request;
+
+class BookController extends Controller
+{
+    public function index() {
+        $books = Book::all();
+        return view("admin.book.index", compact("books"));
+    }
+
+    public function  create(){
+        return view('admin.book.create');
+    }
+
+    public function store(Request $request){
+        $this->validate($request, [
+            'name' => 'required',
+            'author' => 'required',
+            'year' => 'required',
+        ]);
+
+       Book::create($request->all());
+
+       return redirect()->route("book-index");
+    }
+
+    public function edit($id){
+        $book = Book::where("id", $id)->first();
+        return view("admin.book.edit", compact("book"));
+    }
+
+    public function update(Request $request, $id){
+        $book = Book::where("id", $id)->first();
+
+        $this->validate($request, [
+            'name' => 'required',
+            'author' => 'required',
+            'year' => 'required',
+        ]
+        );
+
+        $book->update($request->all());
+
+        return redirect()->route("book-index");
+    }
+
+    public function destroy($id){
+        $book = Book::where("id", $id)->first();
+        $book->delete();
+
+        return redirect()->route("book-index");
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\Admin\BookController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/book/create', [BookController::class, 'create'])->name("book-create");
+Route::get('/book/edit/{id}', [BookController::class, 'edit'])->name("book-edit");
+Route::post('/book/store', [BookController::class, 'store'])->name("book-store");
+Route::get('/book/index', [BookController::class, 'index'])->name("book-index");
+Route::delete('/book/delete/{id}', [BookController::class, 'destroy'])->name("book-delete");
+Route::put('/book/update/{id}', [BookController::class, 'update'])->name("book-update");
