4d0206236680842ab6ee19d3c91f894e5c41efc5
sulkifli27 <sulgameranimasi1@gmail.com>
2023-01-22 10:32:37 +0800

    balde

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\BookController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,3 +25,7 @@ Route::post('/book/store', [BookController::class, 'store'])->name("book-store")
 Route::get('/book/index', [BookController::class, 'index'])->name("book-index");
 Route::delete('/book/delete/{id}', [BookController::class, 'destroy'])->name("book-delete");
 Route::put('/book/update/{id}', [BookController::class, 'update'])->name("book-update");
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
